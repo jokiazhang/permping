f6082bbe09074c57ebe00130a68058d33aa213ce
code2die <code2die@gmail.com>
2012-05-13 11:12:46 +0000

    Models

---
+++ android/src/com/permping/model/PermImage.java
@@ -0,0 +1,12 @@
+package com.permping.model;
+
+import java.io.Serializable;
+
+public class PermImage implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2L;
+
+}
