d2e613c33541f7c934ab059dd7df7d2c58f7111f
code2die@gmail.com <code2die@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-13 11:12:46 +0000

    Models

--- trunk/android/src/com/permping/model/Perm.java
+++ trunk/android/src/com/permping/model/Perm.java
@@ -1,7 +1,92 @@
+/**
+ * 
+ */
 package com.permping.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 public class Perm implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String id;
+	private String name;
+	private String description;
+	private PermImage image;
+	private ArrayList<PermComment> comments;
+	
+	
+	/**
+	 * Constructor
+	 */
+	public Perm( String id){
+		this.setId(id);
+	}
+	
+	public Perm( String id, String name, String description, PermImage image ){
+		this.setId(id);
+		this.setName(name);
+		this.setDescription(description);
+		this.setImage(image);
+	}
+	
+	
+	public Perm( String id, String name, String description, PermImage image , ArrayList<PermComment> comments){
+		this.setId(id);
+		this.setName(name);
+		this.setDescription(description);
+		this.setImage(image);
+		this.setComments( comments );
+	}
+	
+	
+	
+	/**
+	 * Getters / Setters
+	 */
+	
+	public void setComments( ArrayList<PermComment> comments){
+		this.comments = comments;
+	}
+	
+	public ArrayList<PermComment> getComments(){
+		return this.comments;
+	}
+
+	public PermImage getImage() {
+		return image;
+	}
+
+	public void setImage(PermImage image) {
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 }
---
+++ trunk/android/src/com/permping/model/PermComment.java
@@ -0,0 +1,12 @@
+package com.permping.model;
+
+import java.io.Serializable;
+
+public class PermComment implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3L;
+
+}
