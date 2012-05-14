c13b898a6349f70db1f2befda27a4da7b167c47f
code2die@gmail.com <code2die@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-14 10:45:58 +0000

    Merge with Linh's sources

--- trunk/android/src/com/permping/adapter/PermAdapter.java
+++ trunk/android/src/com/permping/adapter/PermAdapter.java
@@ -1,17 +1,11 @@
 package com.permping.adapter;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 
 import com.permping.R;
 import com.permping.model.Perm;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
