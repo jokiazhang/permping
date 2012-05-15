56249d3d81444d2ffe8cf762c2a1ad00d9f813d8
linhvn0110 <linhvn0110@gmail.com>
2012-05-15 04:38:49 +0000

    Fix the twitter

--- android/src/com/permping/utils/Constants.java
+++ android/src/com/permping/utils/Constants.java
@@ -12,15 +12,15 @@ public class Constants {
 	/**
 	 * Twitter parameters required by the API
 	 */
-	public static final String CONSUMER_KEY = "9svvOiVkxleiWWcVNWyddg";
-	public static final String CONSUMER_SECRET= "LUXYs5AdpGionGgPDd8nkKOdDbLrj3DAf8YyYgAM1U";
+	public static final String CONSUMER_KEY = "aZNx8rmFPHMMNm8t4kqhmQ";
+	public static final String CONSUMER_SECRET= "P2m0Ow5pKFjql1Uco0aiTeYZ7089fP4s7wmWxanE";
 	
 	public static final String REQUEST_URL = "https://api.twitter.com/oauth/request_token";
 	public static final String ACCESS_URL = "https://api.twitter.com/oauth/access_token";
 	public static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize";
 	
 	public static final String	OAUTH_CALLBACK_SCHEME	= "perm";
-	public static final String	OAUTH_CALLBACK_HOST		= "callback";
-	public static final String	OAUTH_CALLBACK_URL		= OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+	public static final String	OAUTH_CALLBACK_HOST		= "twitter";
+	public static final String	OAUTH_CALLBACK_URL		=  "perm://twitter";
 	
 }
