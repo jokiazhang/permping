76dd955a1955ca7c46eeb3c78ce5a4c97652d90b
linhvn0110 <linhvn0110@gmail.com>
2012-05-15 12:02:16 +0000

    Support Facebook login -> need to check again tonight

--- android/src/com/permping/activity/FollowerActivity.java
+++ android/src/com/permping/activity/FollowerActivity.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import com.permping.R;
 import com.permping.controller.PermListController;
 import com.permping.model.Perm;
-import com.permping.utils.twitter.TwitterUtils;
+import com.permping.utils.facebook.FacebookConnector;
+import com.permping.utils.facebook.SessionEvents;
+import com.permping.utils.facebook.SessionEvents.AuthListener;
+import com.permping.utils.facebook.SessionStore;
 import com.permping.adapter.*;
 import android.view.View.OnClickListener;
 
@@ -14,8 +17,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -28,6 +31,23 @@ public class FollowerActivity extends Activity {
 	private Button login;
 	
 	private String[] list1 = { "Icon", "Icon Creator", "Image", "Image Creator" };
+	
+	/*
+	 * Facebook 
+	 */
+	private static final String FACEBOOK_APP_ID = "164668790213609";
+	private static final String ACCESS_TOKEN = "access_token";
+	private static final String ACCESS_EXPIRES = "access_expires";
+	private static final String EMAIL = "email";
+	private static final String PUBLISH_STREAM = "publish_stream";
+	
+	/*private Facebook facebook;
+	private AsyncFacebookRunner mAsyncRunner;
+	private SharedPreferences sharedPreferences;
+	private Context context;*/
+	
+	private final Handler facebookHander = new Handler();
+	private FacebookConnector facebookConnector;
 	
 	
 	/** Called when the activity is first created. */
@@ -42,7 +62,13 @@ public class FollowerActivity extends Activity {
         
         PermAdapter permListAdapter = new PermAdapter(this, R.layout.perm_item_1, permList);
         permListView.setAdapter(permListAdapter);
-                
+        
+        // Initialize the facebook connection
+        facebookConnector = new FacebookConnector(FACEBOOK_APP_ID, this, getApplicationContext(), new String[] {EMAIL, PUBLISH_STREAM});
+        
+        //facebook = new Facebook(APP_ID);
+        //mAsyncRunner = new AsyncFacebookRunner(facebook);
+        
         join = (Button) findViewById(R.id.bt_join);
         login = (Button) findViewById(R.id.bt_login);
         
@@ -73,7 +99,57 @@ public class FollowerActivity extends Activity {
 		//updateLoginStatus();
 	}
     
-    
+
+	/**
+     * Support to login to Facebook by opening facebook login window.
+     *//*
+    private void loginToFacebook() {
+    	sharedPreferences = getPreferences(MODE_PRIVATE);
+		String access_token = sharedPreferences.getString(ACCESS_TOKEN, null);
+		long expires = sharedPreferences.getLong(ACCESS_EXPIRES, 0);
+		
+		if (access_token != null) {
+			facebook.setAccessToken(access_token);
+		}
+		
+		if (expires != 0) {
+			facebook.setAccessExpires(expires);
+		}
+		
+		if (!facebook.isSessionValid()) {
+			facebook.authorize(this, new String[] {EMAIL, PUBLISH_STREAM}, 
+					new Facebook.DialogListener() {
+				
+				@Override
+				public void onFacebookError(FacebookError e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onError(DialogError e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onComplete(Bundle values) {
+					// Function to handle complete event
+					// Edit Preferences and update facebook access token
+					SharedPreferences.Editor editor = sharedPreferences.edit();
+					editor.putString(ACCESS_TOKEN, facebook.getAccessToken());
+					editor.putLong(ACCESS_EXPIRES, facebook.getAccessExpires());
+					editor.commit();
+				}
+				
+				@Override
+				public void onCancel() {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+	}*/
     
     /**
      * This listener is to handle the click action of Join button
@@ -109,30 +185,46 @@ public class FollowerActivity extends Activity {
     	
     	public void onClick(View v) {
 			if (v == facebookLogin) {
-				
-			} else if (v == twitterLogin) {
-				/*if (!TwitterUtils.isAuthenticated(prefs)) {
-					Intent i = new Intent(getContext(), PrepareRequestTokenActivity.class);
-					//i.putExtra("tweet_msg", getTweetMsg());
-					getContext().startActivity(i);
-				} else {
+				if (!facebookConnector.getFacebook().isSessionValid()) {
+					AuthListener authListener = new AuthListener() {
+						
+						@Override
+						public void onAuthSucceed() {							
+							//Edit Preferences and update facebook access token
+							SharedPreferences.Editor editor = prefs.edit();
+							editor.putString(ACCESS_TOKEN, facebookConnector.getFacebook().getAccessToken());
+							editor.putLong(ACCESS_EXPIRES, facebookConnector.getFacebook().getAccessExpires());
+							editor.commit();
+						}
+						
+						@Override
+						public void onAuthFail(String error) {
+							// TODO Auto-generated method stub							
+						}
+					};
 					
-				}*/
-				
+					SessionEvents.addAuthListener(authListener);
+					facebookConnector.login();
+				}
+				this.dismiss();
+			} else if (v == twitterLogin) {
 				Intent i = new Intent(getContext(), PrepareRequestTokenActivity.class);
-				//i.putExtra("tweet_msg", getTweetMsg());
 				getContext().startActivity(i);
+				this.dismiss();
 				
 			} else { // Show Join Permping screen
-				// TODO Auto-generated method stub
 				Intent i = new Intent(getContext(), JoinPermActivity.class);
 				getContext().startActivity(i);
-			}
+				this.dismiss();
+			}			
 		}
     	
+    	
+    	
+    	/*
     	private String getTweetMsg() {
     		return "Tweeting from Android App at " + new DateFormat().toString();
-    	}	
+    	}*/	
     }
     
 }
