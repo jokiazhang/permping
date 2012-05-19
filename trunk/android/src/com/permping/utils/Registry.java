49bbae7f35af9682eb7fcfb715691c9ae430f381
code2die@gmail.com <code2die@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-19 09:40:22 +0000

    Scroll buttons & move listeners

---
+++ trunk/android/src/com/permping/utils/Registry.java
@@ -0,0 +1,10 @@
+package com.permping.utils;
+
+import com.permping.PermpingMain;
+
+import android.content.Context;
+
+public class Registry {
+	public static Boolean popularPermFirst = true;
+	
+}
