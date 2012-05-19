c0501c02f4dc120d6df7da11174bd2122fc6bbfc
linhvn0110@gmail.com <linhvn0110@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-19 16:22:56 +0000

    Fix the login bug

--- trunk/android/src/com/permping/activity/FollowerActivity.java
+++ trunk/android/src/com/permping/activity/FollowerActivity.java
@@ -1,42 +1,17 @@
 package com.permping.activity;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import com.permping.R;
 import com.permping.controller.PermListController;
 import com.permping.model.Perm;
-import com.permping.utils.facebook.FacebookConnector;
-import com.permping.utils.facebook.SessionEvents;
-import com.permping.utils.facebook.SessionEvents.AuthListener;
-//import com.permping.utils.facebook.SessionStore;
 import com.permping.adapter.*;
-import android.view.View.OnClickListener;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-//import android.os.Handler;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 
 public class FollowerActivity extends Activity {
-	// The Join button
-	private Button join;
-	// The Login button
-	private Button login;
-	
-	private String[] list1 = new String[] { "Icon", "Icon Creator", "Image", "Image Creator" };
-
-	
-	
-	
 		
 	/** Called when the activity is first created. */
     @Override
@@ -51,40 +26,10 @@ public class FollowerActivity extends Activity {
         PermAdapter permListAdapter = new PermAdapter(this, R.layout.perm_item_1, permList, this);
         permListView.setAdapter(permListAdapter);
         
-        
-        /*
-        // Initialize the facebook connection
-        facebookConnector = new FacebookConnector(FACEBOOK_APP_ID, 
-        		this, getApplicationContext(), new String[] {EMAIL, PUBLISH_STREAM});
-        
-        join = (Button) findViewById(R.id.bt_join);
-        login = (Button) findViewById(R.id.bt_login);
-        
-        login.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent i = new Intent(v.getContext(), LoginPermActivity.class);
-				v.getContext().startActivity(i);
-			}
-		});
-        
-        final OptionsDialog dialog = new OptionsDialog(this);
-		
-        // Show the dialog
-		join.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				dialog.show();
-			}
-		}); 
-		*/
     }
     
     @Override
 	protected void onResume() {
 		super.onResume();
 	}
-    
-    
-    
 }
