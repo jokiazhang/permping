6dfd27b684e8a0c6314303a00d91b0a2ecb8ef94
code2die <code2die@gmail.com>
2012-05-21 18:36:59 +0000

    image layout

--- android/src/com/permping/activity/ImageActivity.java
+++ android/src/com/permping/activity/ImageActivity.java
@@ -4,10 +4,21 @@ import com.permping.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ImageActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_layout);
+        
+        final LinearLayout actionHide = (LinearLayout) findViewById(R.id.takePhoto);
+        actionHide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("AAAAAAAAAAAAAAAAAA", "Event caught");
+            }
+        });
+        
     }
 }
