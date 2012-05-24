948fb934bb3b545db2f6ab91126d6955b5fa0fec
linhvn0110@gmail.com <linhvn0110@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-24 03:27:27 +0000

    Fill profile data

--- trunk/android/src/com/permping/activity/ProfileActivity.java
+++ trunk/android/src/com/permping/activity/ProfileActivity.java
@@ -1,13 +1,58 @@
 
 package com.permping.activity;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.permping.PermpingApplication;
 import com.permping.R;
+import com.permping.model.PermBoard;
+import com.permping.model.PermImage;
+import com.permping.model.User;
+import com.permping.utils.UrlImageViewHelper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_layout);
+        
+        ImageView authorAvatar = (ImageView) findViewById(R.id.authorAvatar);
+        TextView authorName = (TextView) findViewById(R.id.authorName);
+        TextView authorProp = (TextView) findViewById(R.id.authorProp);
+        
+        /** Load the information from Appliaction (user info) when the page is loaded. */
+        PermpingApplication state = (PermpingApplication) getApplicationContext();
+        User user = state.getUser();
+        if (user != null) {
+        	String name = user.getName();
+            PermImage avatar = user.getAvatar();
+            String permNo = String.valueOf(user.getBoard());
+            String friends = String.valueOf(user.getFriends());
+            List<PermBoard> boards = user.getBoards();
+            
+            /** Get the component Ids to fill the data */
+            // The author avatar on the top left.
+            // UrlImageViewHelper.setUrlDrawable(authorAvatar, avatar.getUrl());
+            UrlImageViewHelper.setUrlDrawable(authorAvatar, "http://4.bp.blogspot.com/_n3ecP8ZNh28/TEYB2wyIJDI/AAAAAAAAACw/Fry7Qdpvbzo/s1600/390261_f520.jpg");
+            
+            // The author name
+            authorName.setText(name);
+            
+            // The info below the author name
+            authorProp.setText("N/A perms followers " + friends);
+        } else {
+        	/** User is not authorized yet -> forward to login screen */
+        	Toast toast = Toast.makeText(getApplicationContext(), "User is not authorized! Please do it.", Toast.LENGTH_SHORT);
+        	toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 30);
+        	toast.show();
+        }
+        
     }
 }
