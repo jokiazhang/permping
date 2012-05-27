efa3159342592659aa57e4477f672931781017a8
linhvn0110@gmail.com <linhvn0110@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-05-27 19:32:37 +0000

    update profile tab

--- trunk/android/src/com/permping/activity/ProfileActivity.java
+++ trunk/android/src/com/permping/activity/ProfileActivity.java
@@ -10,12 +10,17 @@ import com.permping.controller.BoardController;
 import com.permping.model.PermBoard;
 import com.permping.model.PermImage;
 import com.permping.model.User;
+import com.permping.utils.PermUtils;
 import com.permping.utils.UrlImageViewHelper;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,51 +28,71 @@ import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
 	
+	ImageView authorAvatar;
+	TextView authorName;
+	TextView friends;
+	TextView followings;
+	
+	private int selectedBoardId = -1;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_layout);
         
-        ImageView authorAvatar = (ImageView) findViewById(R.id.authorAvatar);
-        TextView authorName = (TextView) findViewById(R.id.authorName);
-        TextView friends = (TextView) findViewById(R.id.friends);
-        TextView followings = (TextView) findViewById(R.id.followings);
+        authorAvatar = (ImageView) findViewById(R.id.authorAvatar);
+        authorName = (TextView) findViewById(R.id.authorName);
+        friends = (TextView) findViewById(R.id.friends);
+        followings = (TextView) findViewById(R.id.followings);
         
         /** Load the information from Appliaction (user info) when the page is loaded. */
-        
-        PermpingApplication state = (PermpingApplication) getApplicationContext();
-        User user = state.getUser();
+        User user = PermUtils.isAuthenticated(getApplicationContext());
         if (user != null) {
+        	// The author name
         	String name = user.getName();
-            PermImage avatar = user.getAvatar();
-            //String permNo = String.valueOf(user.getPin());
-            String fr = String.valueOf(user.getFriends());
-            List<PermBoard> boards = user.getBoards();
-            
-            // Get the component Ids to fill the data 
-            // The author avatar on the top left.
-            UrlImageViewHelper.setUrlDrawable(authorAvatar, avatar.getUrl());
-            //UrlImageViewHelper.setUrlDrawable(authorAvatar, "http://4.bp.blogspot.com/_n3ecP8ZNh28/TEYB2wyIJDI/AAAAAAAAACw/Fry7Qdpvbzo/s1600/390261_f520.jpg");
-            
-            // The author name
             authorName.setText(name);
             
-            // The info below the author name
-            friends.setText(fr + " friends");
+            // The author avatar
+        	PermImage avatar = user.getAvatar();
+            UrlImageViewHelper.setUrlDrawable(authorAvatar, avatar.getUrl());
+            
+            // The number of friends
+            friends.setText(String.valueOf(user.getFriends()) + " friends ");
+            
+            // The number of followings
             followings.setText(String.valueOf(user.getFollowings() + " followings"));
+            
+            // Build the list of user's boards
+            ListView userBoards = (ListView) findViewById(R.id.userBoards);
+            ArrayList<PermBoard> boards = (ArrayList<PermBoard>) user.getBoards();
+            BoardAdapter boardAdapter = new BoardAdapter(this,R.layout.board_item, boards);
+            userBoards.setAdapter(boardAdapter);
+            userBoards.setOnItemClickListener(new BoardClickListener());
+            
         } else {
         	// User is not authorized yet -> forward to login screen
         	Toast toast = Toast.makeText(getApplicationContext(), "User is not authorized! Please do it.", Toast.LENGTH_SHORT);
         	toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 50);
         	toast.show();
-        }
-                
-        /** Build the list of boards using ListAdapter */
-        ListView userBoards = (ListView) findViewById(R.id.userBoards);
-        BoardController boardController = new BoardController();
-		final ArrayList<PermBoard> boards = boardController.getUserBoads();
-        BoardAdapter boardAdapter = new BoardAdapter(this,R.layout.board_item, boards  );
-        userBoards.setAdapter(boardAdapter);
-        
-        
+        }        
+    }
+    
+    private class BoardClickListener implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int pos,
+				long id) {
+			PermBoard board = (PermBoard) parent.getItemAtPosition(pos);
+			selectedBoardId = Integer.parseInt(board.getId());
+			BoardController boardController = new BoardController();
+			ArrayList<PermBoard> perms = boardController.getPermByBoardId(board.getId());
+			System.out.println(perms.size());
+		}
+
+		/*@Override
+		public void onNothingClick(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+			
+		}*/
+    	
     }
 }
