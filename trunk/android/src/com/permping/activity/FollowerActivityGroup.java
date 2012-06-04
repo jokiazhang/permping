a2859cba416a90b4d04512a69d1529eac11bb5f9
linhvn0110@gmail.com <linhvn0110@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-06-04 18:36:32 +0000

    Fix the bug of Join

--- trunk/android/src/com/permping/activity/FollowerActivityGroup.java
+++ trunk/android/src/com/permping/activity/FollowerActivityGroup.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 
 public class FollowerActivityGroup extends TabGroupActivity {
-
+	
+	public static FollowerActivityGroup context;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.context = this;
 		View view = getLocalActivityManager().startActivity( "FollowerActivity", new Intent(this, FollowerActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)).getDecorView();
 		replaceView(view);
 	}
