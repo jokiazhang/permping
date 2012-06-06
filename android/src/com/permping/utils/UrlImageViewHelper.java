15cf0887e2463f55379b3b0ebc7258e22de1660b
linhvn0110 <linhvn0110@gmail.com>
2012-06-06 16:39:50 +0000

    fix out of memory

--- android/src/com/permping/utils/UrlImageViewHelper.java
+++ android/src/com/permping/utils/UrlImageViewHelper.java
@@ -44,6 +44,8 @@ public final class UrlImageViewHelper {
         }
         return total;
     }
+    
+    static int IMAGE_MAX_SIZE = 100;
 
     static Resources mResources;
     static DisplayMetrics mMetrics;
@@ -196,7 +198,7 @@ public final class UrlImageViewHelper {
         File file = context.getFileStreamPath(filename);
         if (file.exists()) {
             try {
-            	int IMAGE_MAX_SIZE = 700;
+            	
                 if (cacheDurationMs == CACHE_DURATION_INFINITE || System.currentTimeMillis() < file.lastModified() + cacheDurationMs) {
                     //Log.i(LOGTAG, "File Cache hit on: " + url + ". " + (System.currentTimeMillis() - file.lastModified()) + "ms old.");
                     FileInputStream  fis = context.openFileInput(filename);
@@ -290,7 +292,37 @@ public final class UrlImageViewHelper {
                     fos.close();
                     is.close();
                     FileInputStream  fis = context.openFileInput(filename);
-                    return loadDrawableFromStream(context, fis);
+
+                                   
+                    //Decode image size
+                      BitmapFactory.Options o = new BitmapFactory.Options();
+                      o.inJustDecodeBounds = true;
+                      BitmapFactory.decodeStream(fis, null, o);
+                      fis.close();
+                      
+                      int scale = 1;
+                      if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE) {
+                          scale = (int)Math.pow(2.0, (int) Math.round(Math.log(IMAGE_MAX_SIZE / (double) Math.max(o.outHeight, o.outWidth)) / Math.log(0.5)));
+                      }
+
+                    //Decode with inSampleSize
+                      BitmapFactory.Options o2 = new BitmapFactory.Options();
+                      o2.inSampleSize = scale;
+                      fis = context.openFileInput(filename);
+                      Bitmap b = BitmapFactory.decodeStream(fis, null, o2);
+                      fis.close();
+
+                      prepareResources(context);
+                      //BitmapDrawable drawable = loadDrawableFromStream(context, fis);
+                      BitmapDrawable drawable = new BitmapDrawable(mResources, b);
+
+                    
+                    
+                    
+                    
+                    
+                      return drawable;
+                 //   return loadDrawableFromStream(context, fis);
                 }
                 catch (Exception ex) {
 //                    Log.e(LOGTAG, "Exception during Image download of " + url, ex);
