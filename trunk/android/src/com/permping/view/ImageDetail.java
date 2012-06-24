1068c96447c70fc3a19f556e9824d9be22ec8cb7
thienuit@gmail.com <thienuit@gmail.com@8990626e-befe-ff37-e494-9192f87714fb>
2012-06-24 01:38:04 +0000

    add new file

---
+++ trunk/android/src/com/permping/view/ImageDetail.java
@@ -0,0 +1,69 @@
+package com.permping.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.custom.WebImageView;
+import com.permping.R;
+import com.permping.activity.FollowerActivity;
+
+public class ImageDetail extends Activity {
+	private String url;
+	private Button btnClose;
+	private WebImageView webImageView;
+	public ImageDetail() {
+		// TODO Auto-generated constructor stub
+
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setStyle(DialogFragment.STYLE_NO_TITLE, STYLE_NO_TITLE);
+//		setStyle(DialogFragment.STYLE_NO_FRAME, DialogFragment.STYLE_NORMAL);
+//		View v = LayoutInflater.from(getParent()).inflate(R.layout.image_detail, null);
+		setContentView(R.layout.image_detail);
+		FollowerActivity.isRefesh = false;
+		this.url = getIntent().getExtras().getString("url");
+		btnClose = (Button)findViewById(R.id.btnClose);
+		webImageView = (WebImageView)findViewById(R.id.thumbnail);
+		if(url != null){
+			webImageView.setImageUrl(url);
+			webImageView.loadImage();
+		}
+		btnClose.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+
+	}
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//			Bundle savedInstanceState) {
+//		View v = LayoutInflater.from(getActivity().getParent()).inflate(R.layout.image_detail, null);
+//		btnClose = (Button)v.findViewById(R.id.btnClose);
+//		webImageView = (WebImageView)v.findViewById(R.id.thumbnail);
+//		if(url != null){
+//			webImageView.setImageUrl(url);
+//			webImageView.loadImage();
+//		}
+//		btnClose.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View arg0) {
+//				// TODO Auto-generated method stub
+//				dismiss();
+//			}
+//		});
+//		return v;
+//	}
+
+}
