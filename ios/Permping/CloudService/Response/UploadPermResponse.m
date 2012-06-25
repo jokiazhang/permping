7eb5e1b735ccf8dc7c9f41b374cb4e311b8a83c0
dttuan2004 <dttuan2004@gmail.com>
2012-06-25 07:14:06 +0000

    - update share kakao link

--- ios/Permping/CloudService/Response/UploadPermResponse.m
+++ ios/Permping/CloudService/Response/UploadPermResponse.m
@@ -9,11 +9,29 @@
 #import "UploadPermResponse.h"
 
 @implementation UploadPermResponse
-@synthesize permId;
+@synthesize permId, permIphoneLink, permAndroidLink;
 
 - (void)dealloc {
     self.permId = nil;
     [super dealloc];
+}
+
+- (void)foundCDATA:(NSData *)CDATABlock onPath:(NSString *)path
+{
+    [super foundCDATA:CDATABlock onPath:path];
+    
+    if ([@"/respond/error/permIphoneLink" isEqualToString:path]) 
+	{
+        NSString *text = [[NSString alloc] initWithData:CDATABlock encoding:NSUTF8StringEncoding];
+        self.permIphoneLink = [[text copy] autorelease];
+        [text release];
+	} else if ([@"/respond/error/permAndroidLink" isEqualToString:path]) 
+	{
+        NSString *text = [[NSString alloc] initWithData:CDATABlock encoding:NSUTF8StringEncoding];
+        self.permAndroidLink = [[text copy] autorelease];
+        [text release];
+	}
+    return;
 }
 
 - (void) onEndElement:(NSString *)path name:(NSString *)name text:(NSString *)text
