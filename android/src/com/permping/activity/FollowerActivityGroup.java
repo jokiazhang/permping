6454bacac3692cffd01cfa4cac110e9fc032aa25
buuhoang85 <buuhoang85@gmail.com>
2012-07-14 18:32:53 +0000

    Fix issue 5, 6 and Time out

--- android/src/com/permping/activity/FollowerActivityGroup.java
+++ android/src/com/permping/activity/FollowerActivityGroup.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
+import com.permping.PermpingApplication;
 import com.permping.TabGroupActivity;
 import com.permping.controller.AuthorizeController;
 import com.permping.interfaces.Login_delegate;
+import com.permping.model.User;
 import com.permping.utils.Constants;
 import com.permping.utils.XMLParser;
 
@@ -26,14 +28,29 @@ public class FollowerActivityGroup extends TabGroupActivity implements Login_del
 		//View view = getLocalActivityManager().startActivity( "FollowerActivity", new Intent(this, FollowerActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)).getDecorView();
 		setTabGroup(this);
 		String userEmail = XMLParser.getUserEmail(this);
-		String UserPass = XMLParser.getUserPass(this);
-		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(8);
-		nameValuePairs.add(new BasicNameValuePair("type", Constants.LOGIN_TYPE));
-		nameValuePairs.add(new BasicNameValuePair("oauth_token", ""));
-		nameValuePairs.add(new BasicNameValuePair("email", userEmail));
-		nameValuePairs.add(new BasicNameValuePair("password", UserPass));
-		AuthorizeController authorizeController = new AuthorizeController(FollowerActivityGroup.this);
-		authorizeController.authorize(this, nameValuePairs);
+		String userPass = XMLParser.getUserPass(this);
+		long lastTimeLogin = XMLParser.getLastTimeLogin(this);
+		long timeout = System.currentTimeMillis() - lastTimeLogin;
+		if(userEmail.length() > 0 && userPass.length() > 0) {
+			if(timeout < XMLParser.ACCOUNT_TIME_OUT) {
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(8);
+				nameValuePairs.add(new BasicNameValuePair("type", Constants.LOGIN_TYPE));
+				nameValuePairs.add(new BasicNameValuePair("oauth_token", ""));
+				nameValuePairs.add(new BasicNameValuePair("email", userEmail));
+				nameValuePairs.add(new BasicNameValuePair("password", userPass));
+				AuthorizeController authorizeController = new AuthorizeController(FollowerActivityGroup.this);
+				authorizeController.authorize(this, nameValuePairs);
+			} else {
+				PermpingApplication state = (PermpingApplication) getApplicationContext();
+				User user = state.getUser();
+				if (user != null) {
+					AuthorizeController authorizeController = new AuthorizeController();
+					authorizeController.logout(user.getId());
+					state.setUser(null);
+					XMLParser.storePermpingAccount(this, "", "");
+				}				
+			}
+		}
 		//replaceView(view);
 	}
 	
