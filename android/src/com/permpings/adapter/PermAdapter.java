c06ff5d50dbfe88cc1ac59178e7123dfc1358467
leduyphong <leduyphong@gmail.com>
2012-07-27 16:23:01 +0000

    phong modified code

--- android/src/com/permpings/adapter/PermAdapter.java
+++ android/src/com/permpings/adapter/PermAdapter.java
@@ -275,14 +275,15 @@ public class PermAdapter extends ArrayAdapter<Perm> implements OnClickListener {
 					gotoMap.setTag(viewId);
 					gotoMap.setOnClickListener(PermAdapter.this);
 					TextView permViewInfo = (TextView)view.findViewById(R.id.permVoiceInfo);
-					if(perm.getLon() ==0 && perm.getLat() == 0){
+//					if(perm.getLon() ==0 && perm.getLat() == 0){
+//						gotoMap.setVisibility(View.INVISIBLE);
+////						permViewInfo.setVisibility(View.GONE);
+//						
+//					}else{
+////						gotoMap.setVisibility(View.VISIBLE);
+////						permViewInfo.setVisibility(View.VISIBLE);
+//					}
 						gotoMap.setVisibility(View.INVISIBLE);
-//						permViewInfo.setVisibility(View.GONE);
-						
-					}else{
-//						gotoMap.setVisibility(View.VISIBLE);
-//						permViewInfo.setVisibility(View.VISIBLE);
-					}
 					
 					if(position == items.size() - 1) {
 						View seperatorLine = (View) view.findViewById(R.id.item_separator);
